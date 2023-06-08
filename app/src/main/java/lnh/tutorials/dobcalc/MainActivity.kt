c0849d00ff5c6db83f4837c148952ea01b685b2e
Lee Nahyun <110441894+ghlnh@users.noreply.github.com>
2023-06-08 16:43:43 +0900

    DatePickerDialog를 위한 내용
    버튼 클릭 시, 오늘 날짜 기준 달력이 나옴. ok누르면 토스트 기능 실행

--- app/src/main/java/lnh/tutorials/dobcalc/MainActivity.kt
+++ app/src/main/java/lnh/tutorials/dobcalc/MainActivity.kt
@@ -7,5 +7,28 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
+        //btnDatePicker가 버튼이름이 아님! 변수명임
+		val btnDatePicker : Button = findViewById(r.id.btnDatePicker)
+
+		//실제 버튼을 클릭 시 발생하는 이벤트 설정(토스트)
+		btnDatePicker.setOnClickListner {
+				clickDatePicker()
+				}
     }
+    fun clickDatePicker(){
+		//DatePickerDialog를 위한 변수들
+		val myCalendar = Calendar.getinstance()
+		val year = myCalendar.get(Calendar.YEAR)
+		val month = myCalendar.get(Calendar.MONTH)
+		val day = myCalendar.get(Calendar.DAY_OF_MONTH)
+		DatePickerDialog(this,
+				 DatePickerDialog.OnDateSetListener{ vies, year, month, dayOfMonth ->
+					Toast.makeText(this,
+								 "Datepicker works", Toast.LENGTH_LONG).show()
+			},
+			year,
+			month,
+			day
+			).show()
 }
