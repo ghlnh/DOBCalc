65051cfe401114ed0f3356e0deb6459143af1a5d
Lee Nahyun <110441894+ghlnh@users.noreply.github.com>
2023-06-09 10:51:40 +0900

    분 단위 나이 계산기 완성
    * 날짜 선택 시, 오늘 자 기준으로 얼마나 살았는지 계산
    * 생일을 입력해야 실행되도록 수정
    * 어제 자까지 생일 입력할 수 있도록 기능 설정 (미래 날짜 선택X)

--- app/src/main/java/lnh/tutorials/dobcalc/MainActivity.kt
+++ app/src/main/java/lnh/tutorials/dobcalc/MainActivity.kt
@@ -7,28 +7,34 @@ class MainActivity : AppCompatActivity() {
 	
 	//텍스트뷰를 지금은 초기화하지 않지만 나중에 변수를 부여할 수 있어 널러블로 설정
 	private var tvSelectedDate : TextView? = null
+	private var tvAgeInMinutes : TextView? = null
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
 
 		//btnDatePicker가 버튼이름이 아님! 변수명임
-		val btnDatePicker : Button = findViewById(r.id.btnDatePicker)
-		tvSelectedDate = findvViewById(R.id.tvSelectedDate)
+		val btnDatePicker : Button = findViewById(R.id.btnDatePicker)
+		tvSelectedDate = findViewById(R.id.tvSelectedDate)
+		tvAgeinMinutes = findViewById(R.id.tvAgeInMinutes)
 		//실제 버튼을 클릭 시 발생하는 이벤트 설정(토스트)
 		btnDatePicker.setOnClickListner {
 				clickDatePicker()
 				}
 		}
 
-	fun clickDatePicker(){
+//다른 클래스에서 clickDatePicker()를 사용하지 못하도록 private로 설정
+	private fun clickDatePicker(){
 		//DatePickerDialog를 위한 변수들
 		val myCalendar = Calendar.getinstance()
 		val year = myCalendar.get(Calendar.YEAR)
 		val month = myCalendar.get(Calendar.MONTH)
 		val day = myCalendar.get(Calendar.DAY_OF_MONTH)		
-		DatePickerDialog(this,
-				 DatePickerDialog.OnDateSetListener{ view, selectedYear, selectedMonth, selectedDayOfMonth ->
+		val dpd = DatePickerDialog(this,
+
+//_를 사용하여 특정 매개변수를 생략 가능
+				 DatePickerDialog.OnDateSetListener{ _, selectedYear, selectedMonth
+																								, selectedDayOfMonth ->
 					Toast.makeText(this,
 //1월을 0으로 보고 계산하기 때문에 월+1 해줘야 함
 							"Year was $selectedYear, month was ${selectedMonth+1}
@@ -40,10 +46,27 @@ class MainActivity : AppCompatActivity() {
 					val sdf = SimpleDateFormat("dd/MM/yyyy",Locale.ENGLISH
 
 					val theDate = sdf.parse(selectedDate)
+				//날짜를 선택해야 실행되도록 함
+				theDate?.let{	
+					val selectedDateInMinutes = theDate.time/ 60000
+					
+					val currentDate = sdf.parse(sdf.format(System.currentTimeMills()))
+					currentDate?.let{
+					val currentDateInMinutes = currentDate.time/ 60000
+
+					val differnceInMinutes = currentDateInMinutes - selectedDateInMinutes
+
+					tvAgeInMinutes?.text = differnceInMinutes.toString()}
+				}		
+			
+				//ms 단위라 60000으로 나눔
 			},
 			year,
 			month,
 			day
-			).show()
+			)
+		//어제 날짜까지만 선택할 수 있게 제한 설정
+			dpd.datePicker.maxDate = System.currentTimeMillis()-86400000
+			dpd.show()
 
 	}
